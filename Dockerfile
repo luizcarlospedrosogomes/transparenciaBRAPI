0378a8b7f9a796a13dd994d86b1c34be4bcd9137
Luiz Carlos Pedroso Gomes <lc.pg@hotmail.com>
2021-10-03 21:48:41 +0000

    dev contanier

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,15 @@
-FROM node:11-alpine
+ARG VARIANT=12
+FROM mcr.microsoft.com/vscode/devcontainers/javascript-node:0-${VARIANT}
 
-WORKDIR /node-app
+# Update args in docker-compose.yaml to set the UID/GID of the "node" user.
+ARG USER_UID=1000
+ARG USER_GID=$USER_UID
+RUN if [ "$USER_GID" != "1000" ] || [ "$USER_UID" != "1000" ]; then groupmod --gid $USER_GID node && usermod --uid $USER_UID --gid $USER_GID node; fi
+
+WORKDIR /workspace
 
 COPY package.json . 
-
+RUN npm install -g sequelize-cli
 RUN npm install --quiet
 
 RUN npm install nodemon -g --quiet
@@ -13,3 +19,4 @@ COPY . .
 EXPOSE 9000
 
 CMD npm start
+CMD sequelize db:migrate
--- src/controllres/OrgaosController.js
+++ src/controllres/OrgaosController.js
@@ -1,14 +1,16 @@
 const db = require('../models');
+const logger = require('../config/logger');
 
 class OrgaosController{ 
 
-    static async create(req, res){
-        console.log("create")
+    static async create(req, res){        
         const data = req.body
         try {
             const orgao = await db.Orgaos.create(data);
+            logger.info('orgao criado:', orgao);
             return res.status(200).json({msg: 'orgao salvo', ...orgao})      
         } catch (error) {            
+            logger.info('erro ao criar', data);
             return res.status(400).json({msg: 'erro ao salvar orgao', data: data, ...error})      
         }
     }
@@ -18,7 +20,8 @@ class OrgaosController{
     }
 
     static async listAll(req, res){
-        return res.status(200).json({msg: 'listAll'})  
+        const orgaos = await db.Orgaos.findAll();        
+        return res.status(200).json(orgaos)  
     }
 }
 
